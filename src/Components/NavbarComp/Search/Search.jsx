9e9932f8433d5031e4d5e450cd15a37aac2e3b9c
OmarElkeshen38 <oelkeshen5@gmail.com>
2023-10-23 01:49:17 +0300

    seselect dom by useRef

--- src/Components/NavbarComp/Search/Search.jsx
+++ src/Components/NavbarComp/Search/Search.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 function Search({query, setQuery}) {
 
+  const inputEl = useRef(null);
 
+  useEffect(function () {
+    function callback(e) {
+
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+    return () => document.addEventListener("keydown", callback);
+  }, [setQuery]);
 
   return (
     <>
@@ -12,6 +27,7 @@ function Search({query, setQuery}) {
         placeholder="Search movies..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        ref={inputEl}
       />
     </>
   );
